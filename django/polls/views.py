59686fdede485704394d6069089910664c41710f
Ssmak05 <159900313+Ssmak05@users.noreply.github.com>
2024-04-23 13:29:26 +0300

    Add files via upload

---
+++ django/polls/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+from .models import Choice
+from django.template import loader
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+def index1(request):
+    return HttpResponse("АБОБУС")
+def detail(request, question_id):
+    Choice_list = Choice.objects.order_by('-pub_date')[:5]
+    context = {'Choice_list': Choice_list}
+    return render(request, 'polls/detail.html', context)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+# Create your views here.
